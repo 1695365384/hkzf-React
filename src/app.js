4fece029d90c1558638861a552c53ab00f84fe44
1695365384 <1695365384@qq.com>
2019-07-20 20:53:36 +0800

    路由

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import CityList from './pages/CityList';
+import React, {lazy, Suspense} from 'react'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+const Home = lazy(() => import('./pages/Home'))
 
 const App = () => {
   return (
     <Router>
-      <div id="app">
-        <Route path="/Home" component={Home} />
-        <Route path="/CityList" component={CityList} />
-      </div>
+      <Suspense fallback={<div className="router-loading">LOADING</div>}>
+        <div id="app">
+          <Route path="/" render={() => <Redirect to="/Home" />} />
+          <Route path="/Home" component={Home} />
+        </div>
+      </Suspense>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import {Button} from 'antd-mobile';
+import React from 'react'
+import {TabBar} from 'antd-mobile'
+import '../../assets/fonts/iconfont.css'
 
+const TOP_BAR_LIST = [
+  {title: '首页', icon: 'icon-my', path: '/home/index'},
+  {title: '首页', icon: 'icon-my', path: '/home/index'},
+  {title: '首页', icon: 'icon-my', path: '/home/index'},
+  {title: '首页', icon: 'icon-my', path: '/home/index'},
+]
 export default class Home extends React.Component {
-  render () {
+  state = {
+    selectedTab: 'redTab',
+  }
+
+  render() {
     return (
-      <div>
-        主页
-        <Button type="primary">登录</Button>
-      </div>
-    );
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white">
+        <TabBar.Item
+          title="我的"
+          key="Life"
+          icon={<i className="iconfont icon-my" />}
+          selectedIcon={<i className="iconfont icon-my" />}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            })
+          }}
+          data-seed="logId"
+        />
+      </TabBar>
+    )
   }
 }
