d0d48e769aaf348b21d3be43d0a495b2c3f6e845
1695365384 <1695365384@qq.com>
2019-07-20 22:51:22 +0800

    路由搞好了

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import React, {lazy, Suspense} from 'react'
+import React, {Suspense} from 'react'
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
-const Home = lazy(() => import('./pages/Home'))
+import home from './pages/Home/Home'
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<div className="router-loading">LOADING</div>}>
         <div id="app">
-          <Route path="/" render={() => <Redirect to="/home" />} />
-          <Route path="/home" component={Home} />
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={home} />
         </div>
       </Suspense>
     </Router>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
+import {TabBar} from 'antd-mobile'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-export default class Home extends React.Component {
+import '../../assets/fonts/iconfont.css'
+import './index.scss'
+
+const Index = lazy(() => import('../Index/Index.js'))
+const CItyList = lazy(() => import('../CityList/index.js'))
+const TOP_BAR_LIST = [
+  {
+    title: '首页',
+    icon: 'icon-ind',
+    path: '/home',
+  },
+  {
+    title: '找房',
+    icon: 'icon-findHouse',
+    path: '/home/list',
+  },
+  {
+    title: '资讯',
+    icon: 'icon-infom',
+    path: '/home/news',
+  },
+  {
+    title: '我的',
+    icon: 'icon-my',
+    path: '/home/profile',
+  },
+]
+export default class HomeRouter extends React.Component {
+  state = {
+    selectedTab: this.props.location.pathname,
+  }
+
+  showTabNavBar = () => {
+    return TOP_BAR_LIST.map(item => {
+      return (
+        <TabBar.Item
+          title={item.title}
+          key={item.title}
+          icon={<i className={`iconfont ${item.icon}`} />}
+          selectedIcon={<i className={`iconfont ${item.icon}`} />}
+          selected={this.state.selectedTab === item.path}
+          onPress={() => {
+            this.props.history.push(item.path)
+            this.setState({
+              selectedTab: item.path,
+            })
+          }}
+          data-seed="logId"
+        />
+      )
+    })
+  }
+
   render() {
-    return <div />
+    return (
+      <div>
+        <Route exact path="/home" component={Index} />
+        <Route path="/home/list" component={CItyList} />
+        <div className="tabBarList">
+          <TabBar tintColor="#21B97A" noRenderContent={true}>
+            {this.showTabNavBar()}
+          </TabBar>
+        </div>
+      </div>
+    )
   }
 }
