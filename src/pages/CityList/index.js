ca30d42b4dc8a058f62154fbabacc5b330f88a1f
1695365384 <1695365384@qq.com>
2019-07-21 22:15:07 +0800

    城市列表搭建

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import {AutoSizer, List} from 'react-virtualized'
+import {NavBar, Icon, Flex} from 'antd-mobile'
+
+import './index.scss'
 
 export default class CityList extends React.Component {
   render() {
     return (
       <div className="CityList">
-        <div className="city_header">头部</div>
-        <div className="city_content">内容</div>
-        <div className="city_aside">侧边</div>
+        <div className="city_header">
+          <NavBar
+            icon={<Icon type="left" style={{color: '#999'}} />}
+            mode="light"
+            style={{backgroundColor: '#f6f5f6'}}>
+            路上看到
+          </NavBar>
+        </div>
+        <div className="city_content">
+          <AutoSizer
+            children={({height, width}) => {
+              return (
+                <List
+                  height={height}
+                  width={width}
+                  rowHeight={100}
+                  rowCount={20}
+                  rowRenderer={() => <div>asd</div>}>
+                  asdf
+                </List>
+              )
+            }}
+          />
+        </div>
+        <Flex className="city_aside">
+          <ul>
+            <li>侧边</li>
+          </ul>
+        </Flex>
       </div>
     )
   }
